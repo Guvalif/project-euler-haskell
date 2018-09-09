8317d9530c3c963cd56fcde8aaf90da01c34edd5
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2018-09-10 00:04:23 +0900

    Fix index of primes to assign the answer

--- problem-0007.hs
+++ problem-0007.hs
@@ -2,13 +2,13 @@ isPrime :: Integer -> Bool
 isPrime n =
     let sieve n'
             | n' * n' > n     = True
-            | n `rem` n' == 0 = False
+            | n `mod` n' == 0 = False
             | otherwise       = sieve (n' + if n' == 2 then 1 else 2)
     in sieve 2
 
 
 primes = filter isPrime [ 2 .. ]
-answer = primes !! 10001
+answer = primes !! 10000
 
 
 -- Application Entry Point
