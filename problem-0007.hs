a54746d63c0dd36a537464f83d99ec6391b2b460
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2018-09-09 15:39:38 +0900

    Add problem-0007.hs

---
+++ problem-0007.hs
@@ -0,0 +1,17 @@
+factors :: Integer -> [Integer]
+factors n = [ x | x <- [2 .. n - 1], n `mod` x == 0 ]
+
+
+is_prime :: Integer -> Bool
+is_prime n = case factors n of
+    []     -> True
+    x : xs -> False
+
+
+primes = filter is_prime [2..]
+answer = head . drop 10000 $ primes
+
+
+-- Application Entry Point
+main :: IO ()
+main = print answer
