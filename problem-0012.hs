6b509cbc8d9fca945cff6d4ff78de806a093d558
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2018-09-10 13:25:04 +0900

    Add problem-0012.hs

---
+++ problem-0012.hs
@@ -0,0 +1,26 @@
+import Data.List (group)
+
+
+factors :: Integer -> [Integer]
+factors n = [ x | x <- [ 1 .. n ], n `mod` x == 0 ]
+
+
+factorize :: Integer -> [Integer]
+factorize 1 = []
+factorize n = let x = (factors n) !! 1 in x : factorize (n `div` x)
+
+
+divisors :: Integer -> Integer
+divisors n = product $ map ((+ 1) . toInteger . length) $ (group . factorize) n
+
+
+nthTriangle :: Integer -> Integer
+nthTriangle n = (n * (n + 1)) `div` 2
+
+
+answer = fst . head $ dropWhile ((< 500) . snd) $ map (\n -> (n, divisors n)) $ map nthTriangle [ 1 .. ]
+
+
+-- Application Entry Point
+main :: IO ()
+main = print answer
